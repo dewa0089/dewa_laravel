7be1c5ba68ccf3b39da1bfc79727c610b1d36581
Crystall22 <newdewa03@gmail.com>
2023-10-17 15:13:23 +0700

    push

--- dewapw2/resources/views/mahasiswa/index.blade.php
+++ dewapw2/resources/views/mahasiswa/index.blade.php
@@ -3,23 +3,23 @@
 
 @section('content')
 
+    <h2>Halaman Mahasiswa MDP</h2>
+
+
+    <table class="table table-striped">
+        <tr>
+            <th>NPM</th>
+            <th>Nama</th>
+        </tr>
+        @foreach ($mahasiswa as $item)
+        <tr>
+            <td>{{$item['npm']}}</td>
+            <td>{{$item['nama']}}</td>
+        </tr>
+
+        @endforeach
+    </table>
+
 @endsection
-<h2>Halaman Mahasiswa MDP</h2>
-
-
-<table class="table table-striped">
-  <tr>
-    <th>NPM</th>
-    <th>Nama</th>
-  </tr>
-  @foreach ($mahasiswa as $item)
-  <tr>
-    <td>{{$item['npm']}}</td>
-    <td>{{$item['nama']}}</td>
-  </tr>
-
-  @endforeach
-</table>
-
 
 
