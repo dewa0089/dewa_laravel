a355d89edb03715e1b4c6db3d0f06e3405db85d3
dewa0089 <newdewa03@gmail.com>
2023-10-24 14:21:58 +0700

    push

---
+++ dewapw2/database/migrations/2023_10_24_071251_create_mahasiswas_table.php
@@ -0,0 +1,33 @@
+<?php
+
+use Illuminate\Database\Migrations\Migration;
+use Illuminate\Database\Schema\Blueprint;
+use Illuminate\Support\Facades\Schema;
+
+return new class extends Migration {
+    /**
+     * Run the migrations.
+     */
+    public function up(): void
+    {
+        Schema::create('mahasiswas', function (Blueprint $table) {
+            $table->uuid('id');
+            $table->primary('id');
+            $table->char('npm', 10);
+            $table->string('nama', 50);
+            $table->string('tmpt_lahir', 50);
+            $table->date('tgl_lahir');
+            $table->uuid('prodi_id');
+            $table->foreign('prodi_id')->references('id')->on('prodis')->restrictOnDelete()->restrictOnUpdate();
+            $table->timestamps();
+        });
+    }
+
+    /**
+     * Reverse the migrations.
+     */
+    public function down(): void
+    {
+        Schema::dropIfExists('mahasiswas');
+    }
+};
