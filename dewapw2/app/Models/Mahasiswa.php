4789bd3a603b7f19f9511fa4c1d898fe577283ea
dewa0089 <newdewa03@gmail.com>
2023-10-26 15:07:48 +0700

    push

--- dewapw2/app/Models/Mahasiswa.php
+++ dewapw2/app/Models/Mahasiswa.php
@@ -2,10 +2,18 @@
 
 namespace App\Models;
 
+use Illuminate\Database\Eloquent\Concerns\HasUlids;
 use Illuminate\Database\Eloquent\Factories\HasFactory;
 use Illuminate\Database\Eloquent\Model;
 
 class Mahasiswa extends Model
 {
-    use HasFactory;
+    use HasFactory, HasUlids;
+    protected $table = 'mahasiswas';
+
+    public function prodi()
+    {
+        return $this->belongsTo(Prodi::class, 'prodi_id');
+    }
+
 }
