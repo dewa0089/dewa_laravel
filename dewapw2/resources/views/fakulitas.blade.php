610b72026f757742d1852fb60327987967445791
dewa0089 <newdewa03@gmail.com>
2023-10-29 16:22:53 +0700

    push

--- dewapw2/resources/views/fakulitas.blade.php
+++
@@ -1,7 +0,0 @@
-@extends('layout.main')
-@section('title', 'Fakulitas')
-
-@section('content')
-
-@endsection
- <h2>Halaman Fakulitas</h2>
--- dewapw2/resources/views/prodi.blade.php
+++
@@ -1,8 +0,0 @@
-@extends('layout.main')
-@section('title', 'Prodi')
-
-@section('content')
-
-@endsection
-<h1>Halaman Prodi Mahasiswa</h1>
-
